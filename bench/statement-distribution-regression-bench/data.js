faa7746eca2919e08c7987a6ae50597e6b40a0dd
github-action-benchmark <github@users.noreply.github.com>
2026-02-04 14:53:46 +0000

    add statement-distribution-regression-bench (customSmallerIsBetter) benchmark result for 30b2aebc07f724c3ce85cb78c08d94ff40a0c3f0

--- bench/statement-distribution-regression-bench/data.js
+++ bench/statement-distribution-regression-bench/data.js
@@ -1,52 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1770213060488,
+  "lastUpdate": 1770216825356,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "statement-distribution-regression-bench": [
-      {
-        "commit": {
-          "author": {
-            "email": "gui.thiolliere@gmail.com",
-            "name": "Guillaume Thiolliere",
-            "username": "gui1117"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "18cd0b4d8ff6a2eea379a3b166fe21aec180778f",
-          "message": "Enable statement store in polkadot-omni-node and polkadot-parachain (#8076)\n\nIn `polkadot-omni-node-lib`, when starting the node, when the runtime\nfor the best hash contains the runtime api: `ValidateStatement`, then we\nstart the statement store.\n\nThe statement store is an off-chain data-store for signed statements\naccessible via RPC and offchain worker.\nIt uses the runtime api to get the allowance associated to an account.\n\nThis takes effect in `polkadot-omni-node` and `polkadot-parachain` and\nany node depending on `polkadot-omni-node-lib`.\n\n</br>\n\n</br>\n\n ----\n\n### Old description:\n\nWe want to enable statement store for people chain.\n\nThis PR introduces and enables the statement store and add a new\nargument `disable_statement_store` in `polkadot-omni-node-lib`.\nThis takes effect in `polkadot-omni-node` and `polkadot-parachain` and\nany node depending on `polkadot-omni-node-lib`.\n\nThe reasoning is following other service such as `offchain_worker` the\ndefault behavior is to support it, otherwise I expect not many collator\nto explictly opt-in (but I can be wrong assuming this).\nDo system chain collator use `omni-node` or `polkadot-parachain`? we\ncould otherwise only enable it for `polkadot-parachain`. Or we could\nalso create a new binary for people chain and enable it only for this\nnew binary, and remove people chain from `polkadot-parachain`.\n\nIf the runtime doesn't support the API then it any submission will fail\nwith `Error calling into the runtime`.\n\nIt would be ideal to target the next cut-off for this issue/PR given\nthat it can take time for node operator to update their node.\n\nTo have a configurable CLI and default behavior for statement-store, I\ndid in this PR: https://github.com/paritytech/polkadot-sdk/pull/8421\n\n---------\n\nCo-authored-by: cmd[bot] <41898282+github-actions[bot]@users.noreply.github.com>\nCo-authored-by: Sebastian Kunert <skunert49@gmail.com>\nCo-authored-by: Iulian Barbu <14218860+iulianbarbu@users.noreply.github.com>",
-          "timestamp": "2025-06-26T14:44:56Z",
-          "tree_id": "9d7792b62cc997aad62d005d4685d62689efae28",
-          "url": "https://github.com/paritytech/polkadot-sdk/commit/18cd0b4d8ff6a2eea379a3b166fe21aec180778f"
-        },
-        "date": 1750953346249,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Sent to peers",
-            "value": 127.95799999999997,
-            "unit": "KiB"
-          },
-          {
-            "name": "Received from peers",
-            "value": 106.39999999999996,
-            "unit": "KiB"
-          },
-          {
-            "name": "statement-distribution",
-            "value": 0.03439692254600001,
-            "unit": "seconds"
-          },
-          {
-            "name": "test-environment",
-            "value": 0.04572482815199996,
-            "unit": "seconds"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -21999,6 +21955,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "statement-distribution",
             "value": 0.03817515655400001,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "paolo@parity.io",
+            "name": "Paolo La Camera",
+            "username": "sigurpol"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "30b2aebc07f724c3ce85cb78c08d94ff40a0c3f0",
+          "message": "pallet-dap: mark funds as inactive  + expect buffer account to be pre-funded (#10957)\n\nIn pallet-dap, mark funds in the issuance buffer as inactive so they do\nnot participate in governance.\nIn production, the buffer account must be pre-funded (e.g., via genesis\nallocation or transfer) before the pallet receives any funds. These\npre-funded tokens are not deactivate. The expectation is that we\npre-fund with ED so this is negligible.\n\nDriven-by: \n- removed the `InitBufferAccount` migration since the buffer account is\nnow expected to be pre-funded externally.\n-  remove DAP dependency from main staking-async crate\n- For delegated staking, redirect slashes to DAP for Westend AssetHub.\n\n---------\n\nCo-authored-by: cmd[bot] <41898282+github-actions[bot]@users.noreply.github.com>",
+          "timestamp": "2026-02-04T13:29:05Z",
+          "tree_id": "6eefa4986f79a63d283698b86fe50864207bd521",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/30b2aebc07f724c3ce85cb78c08d94ff40a0c3f0"
+        },
+        "date": 1770216803431,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Sent to peers",
+            "value": 128.048,
+            "unit": "KiB"
+          },
+          {
+            "name": "Received from peers",
+            "value": 106.39999999999996,
+            "unit": "KiB"
+          },
+          {
+            "name": "statement-distribution",
+            "value": 0.038477490764000005,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.06552691944199994,
             "unit": "seconds"
           }
         ]
